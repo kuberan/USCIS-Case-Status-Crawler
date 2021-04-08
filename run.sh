b48abb6e3e5615e5942248e1d5fd514bd8e9fd5d
Kuberan Marimuthu <1269336+kuberan@users.noreply.github.com>
2021-04-07 18:40:59 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#Sample Script to download the case status for MSC21903x serie
+python gen.py "34" > 34.log &
+python gen.py "35" > 35.log &
+python gen.py "36" > 36.log &
+python gen.py "37" > 37.log &
+python gen.py "38" > 38.log &
